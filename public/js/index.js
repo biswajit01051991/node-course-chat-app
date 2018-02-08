693fa5679347f3bc37ba5f0c9cf6096bc707b5c9
Biswajit Singha <biswasingha.09@gmail.com>
2018-02-08 22:06:05 +0530

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -9,20 +9,38 @@ console.log('Disconnected from server');
 });
 
 socket.on('newMessage',function(message){
-console.log('newMessage',message);
+	var formattedTime=moment(message.createdAt).format('h:mm a');
+	var template=$('#message-template').html();
+	var html=Mustache.render(template,{
+		text:message.text,
+		from:message.from,
+		createdAt:formattedTime
+	});
+	$('#messages').append(html);
+//console.log('newMessage',message);
+/*
+
 
 var li=jQuery('<li></li>');
-li.text(`${message.from}: ${message.text}`);
-jQuery('#messages').append(li);
+li.text(`${message.from}:${formattedTime} ${message.text}`);
+jQuery('#messages').append(li);*/
 });
 
 socket.on('newLocationMessage',function(message){
-	var li=jQuery('<li></li>');
-	var a=jQuery(`<a target="_blank">My current location</a>`);
-	li.text(`${message.from}:`);
-	a.attr('href',message.url);
-	li.append(a);
-	jQuery('#messages').append(li);
+	var formattedTime=moment(message.createdAt).format('h:mm a');
+	var template=$('#location-message-template').html();
+	var html=Mustache.render(template,{
+		from:message.from,
+		url:message.url,
+		createdAt:formattedTime
+	});
+	$('#messages').append(html);
+	//var li=jQuery('<li></li>');
+	//var a=jQuery(`<a target="_blank">My current location</a>`);
+	//li.text(`${message.from} ${formattedTime}:`);
+	//a.attr('href',message.url);
+	//li.append(a);
+	//jQuery('#messages').append(li);
 });
 
 socket.on('welcomeMessage',function(message){
